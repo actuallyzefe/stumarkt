edf3daaec59046167eef6feab86bfe0f17c7180b
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-07-02 04:29:53 +0300

    feat: follow logic implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Patch, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { GetCurrentUserId } from 'src/auth/common/decorators';
+import { FollowLogicDTO } from './dtos/follow-logic.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +13,10 @@ export class UsersController {
   }
 
   @Patch('follow-unfollow')
-  followUnFollowLogic(@GetCurrentUserId() userId: number, @Body() postedUser) {
+  followUnFollowLogic(
+    @GetCurrentUserId() userId: number,
+    @Body() postedUser: FollowLogicDTO,
+  ) {
     return this.usersService.followUnFollowLogic(userId, postedUser);
   }
 }
